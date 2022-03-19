8cbf2c0774ba2e61a69cda148b1c76d995b8db1d
gra-m <91182298+gra-m@users.noreply.github.com>
2022-03-19 10:53:29 +0100

    #15 Vets listed vet controller..index..vetservice

--- pet-clinic-web/src/main/java/fun/madeby/gdpetclinic/controllers/VetController.java
+++ pet-clinic-web/src/main/java/fun/madeby/gdpetclinic/controllers/VetController.java
@@ -1,6 +1,8 @@
 package fun.madeby.gdpetclinic.controllers;
 
+import fun.madeby.gdpetclinic.services.VetService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 /**
@@ -10,9 +12,15 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping("/vets")
 @Controller
 public class VetController {
+    private final VetService VET_SERVICE;
+
+    public VetController(VetService vet_service) {
+        VET_SERVICE = vet_service;
+    }
 
     @RequestMapping({"", "/", "/index", "/index.html" })
-    public String listVets() {
+    public String listVets(Model model) {
+        model.addAttribute("vets", VET_SERVICE.findAll());
        return "vets/index";
     }
 }
