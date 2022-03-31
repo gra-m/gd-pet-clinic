98132ee1c5684f39da3d79736bd04996a5bea214
gra-m <91182298+gra-m@users.noreply.github.com>
2022-03-31 11:27:33 +0200

    closes #34 PetTypeService (Map) created and annotated

---
+++ pet-clinic-data/src/main/java/fun/madeby/gdpetclinic/services/PetTypeService.java
@@ -0,0 +1,11 @@
+package fun.madeby.gdpetclinic.services;
+
+import fun.madeby.gdpetclinic.model.PetType;
+
+/**
+ * Created by Gra_m on 2022 03 31
+ */
+
+public interface PetTypeService extends CrudService<PetType, Long>{
+
+}
---
+++ pet-clinic-data/src/main/java/fun/madeby/gdpetclinic/services/map/PetTypeServiceMap.java
@@ -0,0 +1,39 @@
+package fun.madeby.gdpetclinic.services.map;
+
+import fun.madeby.gdpetclinic.model.PetType;
+import fun.madeby.gdpetclinic.services.PetTypeService;
+import org.springframework.stereotype.Service;
+
+import java.util.Set;
+
+/**
+ * Created by Gra_m on 2022 03 31
+ */
+@Service
+public class PetTypeServiceMap extends AbstractMapService<PetType, Long> implements PetTypeService {
+
+    @Override
+    public Set<PetType> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        super.deleteById(id);
+    }
+
+    @Override
+    public void delete(PetType obj) {
+        super.delete(obj);
+    }
+
+    @Override
+    public PetType save(PetType obj) {
+        return super.save(obj);
+    }
+
+    @Override
+    public PetType findById(Long id) {
+        return super.findById(id);
+    }
+}
