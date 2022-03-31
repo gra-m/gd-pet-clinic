4b373fd5f306409f98bdf1504dffa4218d9a7664
gra-m <91182298+gra-m@users.noreply.github.com>
2022-03-31 12:09:40 +0200

    closes #37 Bootstrapped PetTypes

--- pet-clinic-web/src/main/java/fun/madeby/gdpetclinic/bootstrap/DataLoader.java
+++ pet-clinic-web/src/main/java/fun/madeby/gdpetclinic/bootstrap/DataLoader.java
@@ -1,8 +1,11 @@
 package fun.madeby.gdpetclinic.bootstrap;
 
 import fun.madeby.gdpetclinic.model.Owner;
+import fun.madeby.gdpetclinic.model.PetType;
 import fun.madeby.gdpetclinic.model.Vet;
 import fun.madeby.gdpetclinic.services.OwnerService;
+import fun.madeby.gdpetclinic.services.PetService;
+import fun.madeby.gdpetclinic.services.PetTypeService;
 import fun.madeby.gdpetclinic.services.VetService;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
@@ -16,14 +19,31 @@ public class DataLoader implements CommandLineRunner {
 
     private final OwnerService ownerService;
     private final VetService vetService;
+    private final PetTypeService PET_TYPE_SERVICE;
 
-    public DataLoader(OwnerService ownerService, VetService vetService) {
+    public DataLoader(OwnerService ownerService, VetService vetService, PetTypeService petTypeService) {
         this.ownerService = ownerService;
         this.vetService = vetService;
+        this.PET_TYPE_SERVICE = petTypeService;
     }
 
     @Override
     public void run(String... args) throws Exception {
+
+        PetType petType1 = new PetType();
+        petType1.setName("Pet Type One");
+        PetType petType2 = new PetType();
+        petType2.setName("Pet Type Two");
+        PetType petType3 = new PetType();
+        petType3.setName("Pet Type Three");
+
+        PetType savedPetType1 = PET_TYPE_SERVICE.save(petType1);
+        PetType savedPetType2 = PET_TYPE_SERVICE.save(petType2);
+        PetType savedPetType3 = PET_TYPE_SERVICE.save(petType3);
+
+        System.out.println("Loaded PetTypes...");
+
+
         Owner owner1 = new Owner();
         owner1.setFirstName("Hilbert");
         owner1.setLastName("Humberdick");
