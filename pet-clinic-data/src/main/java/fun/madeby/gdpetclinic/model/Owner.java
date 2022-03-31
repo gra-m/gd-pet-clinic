74fd59b1ba5c2a1e7419e8b5ea00e951cefef309
gra-m <91182298+gra-m@users.noreply.github.com>
2022-03-31 10:39:38 +0200

    closes #31 Visit entity added

--- pet-clinic-data/src/main/java/fun/madeby/gdpetclinic/model/Owner.java
+++ pet-clinic-data/src/main/java/fun/madeby/gdpetclinic/model/Owner.java
@@ -1,8 +1,19 @@
 package fun.madeby.gdpetclinic.model;
 
+import java.util.Set;
+
 /**
  * Created by Gra_m on 2022 03 14
  */
 
 public class Owner extends Person{
+    private Set<Pet> pets;
+
+    public Set<Pet> getPets() {
+        return pets;
+    }
+
+    public void setPets(Set<Pet> pets) {
+        this.pets = pets;
+    }
 }
