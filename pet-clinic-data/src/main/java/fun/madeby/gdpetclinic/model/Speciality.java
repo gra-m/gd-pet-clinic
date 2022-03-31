74a27aac6caf6ff059dadb6797ef58f1835d361b
gra-m <91182298+gra-m@users.noreply.github.com>
2022-03-31 10:45:11 +0200

    closes #32 Speciality & Set<Speciality> created for Vet

---
+++ pet-clinic-data/src/main/java/fun/madeby/gdpetclinic/model/Speciality.java
@@ -0,0 +1,17 @@
+package fun.madeby.gdpetclinic.model;
+
+/**
+ * Created by Gra_m on 2022 03 31
+ */
+
+public class Speciality extends BaseEntity {
+    private String description;
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
--- pet-clinic-data/src/main/java/fun/madeby/gdpetclinic/model/Vet.java
+++ pet-clinic-data/src/main/java/fun/madeby/gdpetclinic/model/Vet.java
@@ -1,8 +1,19 @@
 package fun.madeby.gdpetclinic.model;
 
+import java.util.Set;
+
 /**
  * Created by Gra_m on 2022 03 14
  */
 
 public class Vet extends Person{
+    private Set<Speciality> specialities;
+
+    public Set<Speciality> getSpecialities() {
+        return specialities;
+    }
+
+    public void setSpecialities(Set<Speciality> specialities) {
+        this.specialities = specialities;
+    }
 }
