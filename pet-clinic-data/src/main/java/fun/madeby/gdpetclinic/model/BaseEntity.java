86814fe99ac8cac3832f34cfd6bfc24ab1e1dd75
gra-m <91182298+gra-m@users.noreply.github.com>
2022-04-07 08:35:22 +0200

    .IDENTITY @MappedSuperclass closes #40 BaseEntity

--- pet-clinic-data/src/main/java/fun/madeby/gdpetclinic/model/BaseEntity.java
+++ pet-clinic-data/src/main/java/fun/madeby/gdpetclinic/model/BaseEntity.java
@@ -1,13 +1,20 @@
 package fun.madeby.gdpetclinic.model;
 
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.MappedSuperclass;
 import java.io.Serializable;
 
 /**
  * Created by Gra_m on 2022 03 17
  * This is just so much tidier than all that practice adding id to every model..
  */
-
+@MappedSuperclass
 public class BaseEntity implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     public Long getId() {
