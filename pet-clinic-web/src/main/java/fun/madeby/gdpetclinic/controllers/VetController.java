23c29ba7bdb4fb3704c4c8720e2c89bd180c0de5
gra-m <91182298+gra-m@users.noreply.github.com>
2022-05-03 16:10:15 +0200

    closes #85 Json vets endpoint

--- pet-clinic-web/src/main/java/fun/madeby/gdpetclinic/controllers/VetController.java
+++ pet-clinic-web/src/main/java/fun/madeby/gdpetclinic/controllers/VetController.java
@@ -1,5 +1,6 @@
 package fun.madeby.gdpetclinic.controllers;
 
+import fun.madeby.gdpetclinic.model.Vet;
 import fun.madeby.gdpetclinic.services.VetService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
@@ -7,6 +8,9 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.WebDataBinder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.InitBinder;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.Set;
 
 /**
  * Created by Gra_m on 2022 03 18
@@ -31,5 +35,10 @@ public class VetController {
         log.debug("/vets");
         model.addAttribute("vets", vetService.findAll());
        return "vets/list";
+    }
+
+    @GetMapping("/api/vets")
+    public @ResponseBody Set<Vet> getVetsJson() {
+        return vetService.findAll();
     }
 }
